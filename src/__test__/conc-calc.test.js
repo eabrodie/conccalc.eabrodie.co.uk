15c4c0c90eedf79743092690f4329f95b34f1b16
Eleanor Brodie <eleanorbrodie@Eleanors-MacBook-Pro.local>
2017-07-03 22:40:00 +0100

    Fix test

--- src/__test__/conc-calc.test.js
+++ src/__test__/conc-calc.test.js
@@ -1,6 +1,6 @@
 import ConcCalc from "../conc-calc.js";
 test("conc calc works", () => {
-  expect(ConcCalc(1, "ml", 1, "mg", 0, 0, 0)).toBe(
+  expect(ConcCalc(1, "ml", 1, "mg", 0, 0, 0, "concentration")).toBe(
     "The concentration is 1 mg/ml"
   );
 });
