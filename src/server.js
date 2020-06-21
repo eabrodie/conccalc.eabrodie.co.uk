9069fc525efef9e93b2d8b8a7554918dbbe5a287
Eleanor Brodie <eleanorbrodie@eleanors-mbp.lan>
2020-06-21 19:02:05 +0100

    Add google analytics

--- src/server.js
+++ src/server.js
@@ -7,7 +7,18 @@ const app = express();
 app.get(
   "/",
   (req: express$Request, res: express$Response, next: express$NextFunction) => {
-    res.send('<div id="content"></div><script src="/client.js"></script>');
+    res.send(`
+      <div id="content"></div><script src="/client.js"></script>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170217693-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-170217693-1');
+      </script>
+      `);
   }
 );
 
